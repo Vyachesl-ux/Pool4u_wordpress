cf168d52b4198d7b3343f741426918701de3dda0
Vyacheslav <vyacheslav@headspin.no>
2024-01-18 16:23:31 +0100

    Add markup and functions into projects block

--- wp-content/themes/pool4u/template-parts/blocks/block-projects.php
+++ wp-content/themes/pool4u/template-parts/blocks/block-projects.php
@@ -12,19 +12,28 @@ $projects = new WP_Query([
 
  <div class="wp-block-pool4u-projects<?php echo $class ?>" id="<?php echo $id; ?>">
     <div class="projects" id="projects">
-        <div class="wrapper">
+        <div class="wrapper wide">
             <div class="projects-content">
-                <pre>
+                
                     <?php foreach($projects->posts as $project): $thumbnail = get_the_post_thumbnail($project->ID, 'large') ?>
                         <div class="projects-content__item">
-                            <figure class="projects-content__item-image"><?php echo $thumbnail ?></figure>
-                            <div class="project-content__item-image--info">
-                                <h3 class="project-content__item-image--info-header"><?php echo $project->post_title ?></h3>
+                            <div class="projects-content__item-image">
+                                <?php echo $thumbnail ?>
+                                <div class="projects-content__item-image--info">
+                                    <h3 class="projects-content__item-image--info-header"><?php echo $project->post_title ?></h3>
+                                    <div class="projects-content__item-image--info-description"><?php echo hsl_e("Докладніше") ?></div>
+                                </div>
                             </div>
                         </div>
                     <?php endforeach?>
+                
+                    <div class="projects-content__item">
+                        <h3 class="projects-header"><?php echo get_field("heading")?></h3>
+                        <p class="projects-text"><?php echo get_field("description_action") ?></p>
+                        <div class="btn"><a class="btn-link color" href="#contact_us"><?php echo get_field("button_text") ?></a></div>
+                    </div>
 
-                </pre>
+                
             </div>
         </div>
     </div>
