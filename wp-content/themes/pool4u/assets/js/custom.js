97e68b40cab5c03c411611dfd3598db7281cce75
Vyacheslav <vyacheslav@headspin.no>
2024-01-26 07:42:40 +0100

    Added smoth scroll for page

--- wp-content/themes/pool4u/assets/js/custom.js
+++ wp-content/themes/pool4u/assets/js/custom.js
@@ -5294,3 +5294,22 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
     }
   });
 })();
+"use strict";
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(function (link) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        var targetId = this.getAttribute('href').substring(1);
+        var targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+})();
